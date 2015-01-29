dca6a48c879a62147661c9ae28ffd2d91c895f72
Oscar Marin <oscar.marin@entegreat.com>
2015-01-28 23:26:55 -0600

    Added a new extension method to make easier to read streams as an byte array

---
+++ Code/Luval.Common/StreamExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Common
+{
+    public static class StreamExtension
+    {
+        public static byte[] ReadToEnd(this Stream s)
+        {
+            using (var ms = new MemoryStream())
+            {
+                s.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
