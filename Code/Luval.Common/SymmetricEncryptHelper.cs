36658ae0256996836cad1b14461026963c759e7e
Oscar Marin <oscar@marin.cr>
2015-03-18 10:47:39 -0600

    Added a new class

---
+++ Code/Luval.Common/SymmetricEncryptHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Common
+{
+    public class SymmetricEncryptHelper
+    {
+
+        public SymmetricEncryptHelper(SymmetricAlgorithm algorithm, string key, string iv)
+        {
+            Provider = algorithm;
+            Key = key;
+            IV = iv;
+        }
+
+        public SymmetricAlgorithm Provider { get; set; }
+        public string Key { get; set; }
+        public string IV { get; set; }
+
+    }
+}
